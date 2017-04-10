26f6232affb69751800861d32ee96ce0f0f494f1
GuillaumeDua <guillaume.dua@gmail.com>
2017-04-10 13:06:38 +0200

    gcl::test : sfinae check on test pack

--- Sources/GSE/gcl/test/event.hpp
+++ Sources/GSE/gcl/test/event.hpp
@@ -34,81 +34,92 @@ namespace gcl
 				int c_var = 0;
 			};
 
-			static void handler()
+			struct handler
 			{
-				std::unique_ptr<gcl_event_t::handler> handler(new gcl_event_t::many_to_one_handler
+				static void proceed()
 				{
-					{ gcl::type_info::id<B_event>::value, [](const gcl_event_t::interface_t & ev) { std::cout << "B interface_t catch" << std::endl; } },
-					{ gcl::type_info::id<C_event>::value, [](const gcl_event_t::interface_t & ev) { std::cout << "C interface_t catch" << std::endl; } }
-				});
+					std::unique_ptr<gcl_event_t::handler> handler(new gcl_event_t::many_to_one_handler
+					{
+						{ gcl::type_info::id<B_event>::value, [](const gcl_event_t::interface_t & ev) { std::cout << "B interface_t catch" << std::endl; } },
+						{ gcl::type_info::id<C_event>::value, [](const gcl_event_t::interface_t & ev) { std::cout << "C interface_t catch" << std::endl; } }
+					});
 
-				handler->add_listener<A_event>([](const gcl_event_t::interface_t & ev) { std::cout << "A interface_t catch" << std::endl; });
-				handler->on(A_event{});
+					handler->add_listener<A_event>([](const gcl_event_t::interface_t & ev) { std::cout << "A interface_t catch" << std::endl; });
+					handler->on(A_event{});
 
-				gcl_event_t::interface_t * ev = new B_event();
-				handler->on(gcl::type_info::id<B_event>::value, *ev);
+					gcl_event_t::interface_t * ev = new B_event();
+					handler->on(gcl::type_info::id<B_event>::value, *ev);
 
-				gcl::type_info::holder<gcl_event_t::interface_t> event_value_holder(new C_event());
-				handler->on(event_value_holder);
-			}
-
-			static void dispatcher()
-			{
-				gcl_event_t::dispatcher dispatcher;
-
-				using handler_t = gcl_event_t::handler;
-				using event_t = gcl_event_t::interface_t;
-
-				std::shared_ptr<handler_t> handler1(new gcl_event_t::many_to_one_handler());
-				std::shared_ptr<handler_t> handler2(new gcl_event_t::many_to_many_handler());
-
-				std::cout
-					<< gcl::type_info::id<gcl_event_t::interface_t>::value << " => gcl::type_info::id<gcl_event_t::interface_t>::value" << std::endl
-					<< gcl::type_info::id<A_event>::value << " => gcl::type_info::id<A_event>::value" << std::endl
-					;
-
-				handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler1 : many_to_one_handler : 1 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
-				/*handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler2 : many_to_many_handler  : 1 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
-				handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler2 : many_to_many_handler  : 2 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
-				handler1->add_listener<B_event>(std::move([](const event_t & ev) { std::cout << "B_event -> handler2 : many_to_many_handler  : 3 : " << static_cast<const B_event &>(ev).b_var << std::endl; }));*/
-
-				dispatcher.subscribe<A_event>(handler1);
-				//dispatcher.subscribe<A_event>(handler2);
-
-				std::cout << "dispatching A_event :" << std::endl;
-				dispatcher.dispatch(A_event{ 42 });
-				/*std::cout << "dispatching B_event :" << std::endl;
-				dispatcher.dispatch(B_event{ "Hello, world" });
-				std::cout << "Remove handler1" << std::endl;
-				dispatcher.unsubscribe<A_event>(handler1);
-				std::cout << "dispatching A_event :" << std::endl;
-				dispatcher.dispatch(A_event{ 42 });*/
-			}
-
-			struct experimental
-			{
-				static void static_socket()
-				{
-					struct Event_A {};
-					struct Event_B {};
-					struct Event_C {};
-
-					{	// event_socket
-						gcl_event_t::experimental::static_socket<Event_A, Event_B, Event_C> myEventHandler;
-
-						myEventHandler.add<Event_A>([]() { std::cout << "A event : first cb" << std::endl; });
-						myEventHandler.add<Event_B>([]() { std::cout << "B event : first cb" << std::endl; });
-						myEventHandler.add<Event_C>([]() { std::cout << "C event : first cb" << std::endl; });
-						myEventHandler.add<Event_A>([]() { std::cout << "A event : another cb" << std::endl; });
-						myEventHandler.add<Event_B>([]() { std::cout << "B event : another cb" << std::endl; });
-						myEventHandler.add<Event_C>([]() { std::cout << "C event : another cb" << std::endl; });
-
-						myEventHandler.trigger<Event_B>();
-						myEventHandler.trigger<0>();
-						myEventHandler.trigger(Event_C{});
-					}
+					gcl::type_info::holder<gcl_event_t::interface_t> event_value_holder(new C_event());
+					handler->on(event_value_holder);
 				}
 			};
+			struct dispatcher
+			{
+				static void proceed()
+				{
+					gcl_event_t::dispatcher dispatcher;
+
+					using handler_t = gcl_event_t::handler;
+					using event_t = gcl_event_t::interface_t;
+
+					std::shared_ptr<handler_t> handler1(new gcl_event_t::many_to_one_handler());
+					std::shared_ptr<handler_t> handler2(new gcl_event_t::many_to_many_handler());
+
+					std::cout
+						<< gcl::type_info::id<gcl_event_t::interface_t>::value << " => gcl::type_info::id<gcl_event_t::interface_t>::value" << std::endl
+						<< gcl::type_info::id<A_event>::value << " => gcl::type_info::id<A_event>::value" << std::endl
+						;
+
+					handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler1 : many_to_one_handler : 1 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
+					/*handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler2 : many_to_many_handler  : 1 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
+					handler1->add_listener<A_event>(std::move([](const event_t & ev) { std::cout << "A_event -> handler2 : many_to_many_handler  : 2 : " << static_cast<const A_event &>(ev).a_var << std::endl; }));
+					handler1->add_listener<B_event>(std::move([](const event_t & ev) { std::cout << "B_event -> handler2 : many_to_many_handler  : 3 : " << static_cast<const B_event &>(ev).b_var << std::endl; }));*/
+
+					dispatcher.subscribe<A_event>(handler1);
+					//dispatcher.subscribe<A_event>(handler2);
+
+					std::cout << "dispatching A_event :" << std::endl;
+					dispatcher.dispatch(A_event{ 42 });
+					/*std::cout << "dispatching B_event :" << std::endl;
+					dispatcher.dispatch(B_event{ "Hello, world" });
+					std::cout << "Remove handler1" << std::endl;
+					dispatcher.unsubscribe<A_event>(handler1);
+					std::cout << "dispatching A_event :" << std::endl;
+					dispatcher.dispatch(A_event{ 42 });*/
+				}
+			};
+			struct experimental
+			{
+				struct static_socket
+				{
+					static void proceed()
+					{
+						struct Event_A {};
+						struct Event_B {};
+						struct Event_C {};
+
+						{	// event_socket
+							gcl_event_t::experimental::static_socket<Event_A, Event_B, Event_C> myEventHandler;
+
+							myEventHandler.add<Event_A>([]() { std::cout << "A event : first cb" << std::endl; });
+							myEventHandler.add<Event_B>([]() { std::cout << "B event : first cb" << std::endl; });
+							myEventHandler.add<Event_C>([]() { std::cout << "C event : first cb" << std::endl; });
+							myEventHandler.add<Event_A>([]() { std::cout << "A event : another cb" << std::endl; });
+							myEventHandler.add<Event_B>([]() { std::cout << "B event : another cb" << std::endl; });
+							myEventHandler.add<Event_C>([]() { std::cout << "C event : another cb" << std::endl; });
+
+							myEventHandler.trigger<Event_B>();
+							myEventHandler.trigger<0>();
+							myEventHandler.trigger(Event_C{});
+						}
+					}
+				};
+
+				using pack_t = std::tuple<static_socket>;
+			};
+
+			using pack_t = std::tuple<handler, dispatcher/*, experimental::static_socket*/>;
 
 			static void proceed()
 			{
--- Sources/GSE/gcl/test/test.hpp
+++ Sources/GSE/gcl/test/test.hpp
@@ -5,32 +5,137 @@
 #include "gcl/test/container/polymorphic_vector.hpp"
 #include "gcl/test/type_info.hpp"
 
+#include <type_traits>
+#include <iomanip>
+
+// todo : type_trait : test_component<name>::pass / test_component<name>::fail as constexpr booleans value
+
 namespace gcl
 {
 	namespace test
 	{
-		template <typename ... tunits>
-		static void proceed_one(tunits... tunit_val)
+		struct fail_exception : std::exception {};
+
+		namespace indent_style
 		{
-			std::function<void()> tunit_list[] = { tunit_val... };
+			static const size_t default_name_w = 40;
+		}
+
+		namespace type_traits
+		{
+			template <typename T, typename = void>
+			struct has_pack : std::false_type {};
+			template <typename T>
+			struct has_pack<T, std::void_t<typename T::pack_t>> : std::true_type {};
+
+			template <typename T, typename = void>
+			struct has_proceed : std::false_type {};
+			template <typename T>
+			struct has_proceed<T, std::void_t<decltype(&T::proceed)>> : std::true_type {};
+		}
+
+		template
+		<
+			class ...tunits,
+			template <typename ... tunits> class pack
+		>
+		static void proceed_one(pack<tunits...> &&)
+		{
+			using test_t = std::function<void()>;
+
+			test_t tunit_list[] = { tunits::proceed ... };
 			for (auto & unit : tunit_list)
-				unit();
+			{
+				std::cout << "[+] " << typeid(decltype(unit)).name() << std::endl;
+				//unit();
+			}
+		}
+
+		template <template <class...> class pack, class subcomponent_t, class ... T>
+		static void test_subcomponents(pack<subcomponent_t, T...>)
+		{
+			test_subcomponents(pack<subcomponent_t>{});
+			test_subcomponents(pack<T...>{});
+		}
+
+		template <template <class...> class pack, class subcomponent_t>
+		static void test_subcomponents/*<pack, subcomponent_t>*/(pack<subcomponent_t>)
+		{
+			auto subcomponent_name = typeid(subcomponent_t).name();
+			std::cout << " |- : " << std::left << std::setw(indent_style::default_name_w) << subcomponent_name << " : ";
+			try
+			{
+				//subcomponent_t::proceed();
+				std::cout << "[PASSED]";
+			}
+			catch (const fail_exception & ex)
+			{
+				std::cout << "[FAILED]";
+				std::cout << "    : " << ex.what();
+			}
+			catch (const std::exception & ex)
+			{
+				std::cout << "[CRASHED]";
+				std::cout << "    : " << ex.what();
+			}
+			catch (...)
+			{
+				std::cout << "[CRASHED]";
+				std::cout << "    : FATAL_ERROR";
+			}
+			std::cout << std::endl;
+		}
+
+		template <class component_t>
+		struct component_is
+		{
+			template <bool has_pack = type_traits::has_pack<component_t>::value>
+			static void test()
+			{
+				static_assert(type_traits::has_pack<component_t>::value, "component test pack is mandatory");
+				test_subcomponents(component_t::pack_t{});
+			}
+			template <>
+			static void test<false>()
+			{
+				std::cout << " |- : [No test pack detected]";
+			}
+		};
+
+		template <typename component_t>
+		static void test_component()
+		{
+			auto component_name = typeid(component_t).name();
+
+			std::cout << "[+] : " << std::left << std::setw(indent_style::default_name_w) << component_name << std::endl;
+
+			component_is<component_t>::test();
+
+			std::cout << std::endl;
 		}
 
 		static void proceed()
 		{
 			// todo : test::type return tunits... (test::type::test1, test::type::test2, etc...)
-			/*proceed_one(
-				gcl::test::event::handler,
-				gcl::test::event::dispatcher,
-				gcl::test::event::experimental::static_socket);*/
+			// proceed_one(gcl::test::event::pack_t {});
+			struct toto {};
+			test_component<toto>();
+			test_component<gcl::test::event>();
 
-			// todo : sfinae reflexion check : has_proceed_symbol ? do test : "not implemented"
+			// gcl::test::event::experimental::
+
+			// todo :
+			// sfinae reflexion check : has_proceed_symbol
+			// ? do test
+			// : "not implemented" || is itself a pack ?
+
+			// todo :
+			// proceed() may be void() or bool()
 
 			// todo : clean output
-			gcl::test::event::proceed();
+			/*gcl::test::event::proceed();
 			gcl::test::container::proceed();
-			gcl::test::type_info::experimental::proceed();
+			gcl::test::type_info::experimental::proceed();*/
 		}
 	}
 }
